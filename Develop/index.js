cff36e984c6930ed42f6006739321ea98188caed
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-03-11 21:38:36 -0600

    changed the write to file function again and now it works. Whoooooo!!

--- Develop/index.js
+++ Develop/index.js
@@ -5,74 +5,73 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 
+
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'Title',
-        message: 'What is the title of your project?'
+        message: 'What is the title of your project?',
       },
       {
         type: 'input',
         name: 'Description',
-        message: 'Enter the description of your project?'
+        message: 'Enter the description of your project?',
       },
       {
         type: 'input',
         name: 'Installation',
-        message: 'What is the the installation process for your project?'
+        message: 'What is the the installation process for your project?',
       },
       {
         type: 'input',
         name: 'Usage',
-        message: 'What is the usage of your project?'
+        message: 'What is the usage of your project?',
       },
       {
         type: 'input',
         name: 'Contributions',
-        message: 'Who are the contributors to this project?'
+        message: 'Who are the contributors to this project?',
       },
       {
         type: 'input',
         name: 'License',
-        message: 'What is the license for this project?'
+        message: 'What is the license for this project?',
       },
       {
         type: 'input',
         name: 'Tests',
-        message: 'What are the tests for this project?'
+        message: 'What are the tests for this project?',
       },
       {
         type: 'input',
         name: 'Questions',
-        message: 'What are the questions for this project?'
+        message: 'What are the questions for this project?',
       },
       {
         type: 'input',
         name: 'Username',
-        message: 'What is your GitHub username?'
+        message: 'What is your GitHub username?',
       },
       {
         type: 'input',
         name: 'Email',
-        message: 'What is your email address?'
+        message: 'What is your email address?',
       }
       
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, answers) {
-  const generateMarkdown = require('./utils/generateMarkdown');
-  const readmeContent = generateMarkdown(answers);
-
-  fs.writeFileSync(fileName, readmeContent, 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing README file:', err);
-    } else {
-      console.log('README file generated successfully!');
-    }
-  });
+function writeToFile(fileName, readmeContent) {
+  try {
+    fs.writeFileSync(fileName, readmeContent, 'utf8');
+    console.log('README file generated successfully!');
+  } catch (err) {
+    console.error('Error writing README file:', err);
+  }
 }
+
 
 // TODO: Create a function to initialize app
 function init() {
